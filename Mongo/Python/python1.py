089e1faaa4ba3b515193c57347667df62a25a8c8
itzparfait <niwesep@gmail.com>
2025-05-30 15:22:48 -0700

    First Python file, Arithmetic

---
+++ Mongo/Python/python1.py
@@ -0,0 +1,20 @@
+#Addition
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Adding the numbers
+result = num1 + num2
+
+print("The sum of the numbers is:", result)
+
+#Multiplication
+numa = 24
+numb = 73
+
+#Multiplying the numbers
+
+result = numa * numb
+
+#Showing the results
+
+print("The product is:", result)
